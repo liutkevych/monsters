6afd4e0f51586617722e1427107549fc1e98f21b
liutkevych <oli@keepit.com>
2019-10-18 14:19:14 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {CardList} from './components/card-list/card-list.component';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: [],
+      searchFeild: ''
+    }
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resp => resp.json())
+    .then(users => this.setState({monsters: users}));
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <input 
+          type="search" 
+          placeholder="serach monster" 
+          onChange={e => {
+            this.setState({serachFeild: e.target.value}, () => console.log(this.state))
+          }}
+        />
+        <CardList monsters={this.state.monsters}/>
+      </div>
+    );
+  }
 }
 
 export default App;
