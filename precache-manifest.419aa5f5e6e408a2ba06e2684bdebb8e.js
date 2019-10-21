3c52602159155fe167a87d8d9845e4cf64879336
liutkevych <oli@keepit.com>
2019-10-21 16:03:57 +0300

    Updates

---
+++ precache-manifest.419aa5f5e6e408a2ba06e2684bdebb8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae9f160d2f8699ad52ef2f7e00146550",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "b56abf474996ab59bcd6",
+    "url": "/monsters/static/css/main.12dc2414.chunk.css"
+  },
+  {
+    "revision": "a2079fc446d24f59ff82",
+    "url": "/monsters/static/js/2.462f81e5.chunk.js"
+  },
+  {
+    "revision": "b56abf474996ab59bcd6",
+    "url": "/monsters/static/js/main.1a276808.chunk.js"
+  },
+  {
+    "revision": "0517ab17876bdc46656b",
+    "url": "/monsters/static/js/runtime-main.dd644338.js"
+  }
+]);
